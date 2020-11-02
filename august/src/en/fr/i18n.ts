8749b703241eb237c4686bee354e26edc9b2110e
Olga Yavorska <olga@crowdin.com>
2020-11-02 14:34:30 +0200

    New translations i18n.ts (French)

--- august/src/en/fr/i18n.ts
+++ august/src/en/fr/i18n.ts
@@ -13,7 +13,6 @@ export const translation: Translation = {
     no: `No`,
     next: `Next`,
     back: `Back`,
-    send: `Send`,
     dismiss: `Dismiss`,
     logout: `Logout`,
     retry: `Retry`,
